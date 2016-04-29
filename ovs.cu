2465f3b405b6711d8b88e762e70aa340526b30ff
prupro <pruviola@gmail.com>
2016-04-30 00:41:58 +0530

    ditched. cuFFT can't be called from kernel

--- ovs.cu
+++ ovs.cu
@@ -37,17 +37,14 @@ int main(){
 	fftL = ( 1 << (floorLog(L) + 1) );	
 
 	int ovlapSize = L-1;
-	int extraZeroes;
-	extraZeroes = fftL - ( N%(fftL-ovlapSize) + ovlapSize );
-	if(extraZeroes < ovlapSize) extraZeroes += fftL-ovlapSize;
+	int noOfBlocks = (N+L-1+fftL-1)/(fftL-ovlapSize);
 
 	cudaMemcpy(dev_x,&x,N*sizeof(float),cudaMemcpyHostToDevice);
 	cudaMemcpy(dev_h,&h,L*sizeof(float),cudaMemcpyHostToDevice);
 	cudaMemcpy(dev_y,&y,(N+L-1)*sizeof(float),cudaMemcpyHostToDevice);
 	
 
-
-	dim3 dimGrid(N/fftL,1);
+	dim3 dimGrid(noOfBlocks,1);
 	dim3 dimBlock(fftL,1);
 
 	convolve<<<dimGrid,dimBlock>>>(dev_x dev_h, dev_y,fftL);
@@ -85,13 +82,16 @@ void print(float x[N]){
 __global__ void convolve(float *x, float *h, float *y, int fftL){
 	
 	float zph[fftL] = {0};
-	
+	float zpx[N+L-1+fftL-1] = {0}	
 	int i;
 	for(i=0;i<L;i++)
 		zph[i] = h[i];
 
 	absId = blockIdx.x*blockDim.x+threadIdx.x;
-	zpx[L-1 + absId] =	x[absId];
 
+	if(absId < N) zpx[L-1 + absId] = x[absId];
 	
+	
+	__shared__ float xlocal[fftL] = {0};
+
 }
